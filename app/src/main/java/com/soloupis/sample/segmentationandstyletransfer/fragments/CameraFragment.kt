7a51c26dc2119d83b77a229eb5621c03dde13649
george <george@gigrev.com>
2023-03-04 07:06:42 +0200

    Clean up.

--- app/src/main/java/com/soloupis/sample/segmentationandstyletransfer/fragments/CameraFragment.kt
+++ app/src/main/java/com/soloupis/sample/segmentationandstyletransfer/fragments/CameraFragment.kt
@@ -51,7 +51,7 @@ class CameraFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentCameraBinding.inflate(inflater)
         binding.lifecycleOwner = this
